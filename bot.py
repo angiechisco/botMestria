9a930657943d5a1201884b9323e93c11c6887bfc
angiechisco <chiscoangiedaniela@gmail.com>
2022-11-18 20:53:17 -0500

    Se agrego comando /menu

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 #########################################################
 from config import bot
 import config
+from telebot import types
 from time import sleep
 import re
 bot_data = {}
 class Record:
     def __init__(self):
-    self.height = None
-    self.weight = None
-    self.gender = None
+        self.height = None
+        self.weight = None
+        self.gender = None
 
 # Enable saving next step handlers to file "./.handlers-saves/step.save".
 # Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
@@ -18,8 +19,27 @@ bot.enable_save_next_step_handlers(delay=2)
 # WARNING It will work only if enable_save_next_step_handlers was called!
 bot.load_next_step_handlers()
 
-
 #########################################################
+
+@bot.message_handler(commands=['menu'])
+    def on_command_menu(message):
+    # Using the ReplyKeyboardMarkup class
+    # It's constructor can take the following optional arguments:
+    # - resize_keyboard: True/False (default False)
+    # - one_time_keyboard: True/False (default False)
+    # - selective: True/False (default False)
+    # - row_width: integer (default 3)
+    # row_width is used in combination with the add() function.
+    # It defines how many buttons are fit on each row before continuing on the
+    next row.
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    itembtn1 = types.KeyboardButton('/imc')
+    itembtn2 = types.KeyboardButton('/help')
+    
+    markup.add(itembtn1, itembtn2)
+   
+    bot.send_message(message.chat.id, "Selecciona una opción del menú:",
+    reply_markup=markup)
 
 
 @bot.message_handler(func=lambda message: True)
